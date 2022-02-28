66d76c78c66fb57ce62baaa2e02d5de28b7b0dfe
Ray Anthoney <rmaxey911@gmail.com>
2022-02-28 05:42:37 -0700

    100Devs .js tasks working with variables

--- class14/Variable Tasks/Working with variables/js/main.js
+++ class14/Variable Tasks/Working with variables/js/main.js
@@ -1,11 +1,12 @@
-// Declare two variables: admin and name.
-    var userName = 'Ray Anthoney'
-    console.log(userName)
-    var owner = 'Admin'
-    console.log(owner)
-// Assign the value "John" to name.
-    userName = "John";
-    console.log(userName);
 
+// creating a function allows the word name to not be depracated by the checker in VS Code.
+!(function () { 
+// Declare two variables: admin and name.
+    let admin, name;
+// Assign the value "John" to name.
+    name = "John";
 // Copy the value from name to admin.
+    admin = name;
 // Show the value of admin using alert (must output “John”).
+    alert(admin);
+})();
--- class14/bachelor-code/1/js/main.js
+++ class14/bachelor-code/1/js/main.js
@@ -4,3 +4,11 @@ function hide(){
 	document.querySelector('#claire').style.display = 'none'
 	document.querySelector('#sharleen').style.display = 'none'
 }
+
+
+// document.querySelector('#finalRose').addEventListener('click', hide)
+
+// function hide(){
+// 	document.querySelector('#claire').style.display = 'none'
+// 	document.querySelector('#sharleen').style.display = 'none'
+// }
