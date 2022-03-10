e1091451e5f0d50edb40b428418948e2ecbb3e27
Ray Anthoney <rmaxey911@gmail.com>
2022-03-10 15:59:05 -0700

    100Devs TJSWay practicing chapter 4

--- the-javascript-way/resources/code-skeleton/thejsway-code/chapter04/js/examples.js
+++ the-javascript-way/resources/code-skeleton/thejsway-code/chapter04/js/examples.js
@@ -1,35 +1,37 @@
 /* JavaScript code goes here*/
+console.log("Hello from JavaScript");
 
 /* 
 1. Carousel
-
 Write a program that launches a carousel for 10 turns, showing the turn number each time.
 */
 
-// using the i as a variable that equals 1.
-// (let i = 1;)
-// (i <= 10;) i id then less than or equal to 10.
-// (i++) if i is less than add 1 untill it reaches 10.
-
-// for (let i = 1; i <= 10; i++) {
-// 	console.log(i);
-// }
-
+let i = 1;
+i <= 10;
+i++;
+for (let i = 1; i <= 10; i++) {
+	console.log(i);
+}
 /* When it's done, improve it so that the number of turns is given by the user.*/
-
-// using the word turns as a variable that equals a prompt by the browser.
-// (let turns = promp) Variable turns is now equal to Value prompt.
-// Browser will prompt the user wirh a message ("how many turns should we do?starting with a default value of 0");
-//
 let turns = prompt("how many turns should we do?", 0);
-
-// using the i as a variable that equals 1.
-// (let i = 1;)
-// (i <= turns;) i Variable is now less than or equal to Variable of turns.
-// (i++) if i is less than add 1 untill.
 
 for (let i = 1; i <= turns; i++) {
 	console.log(i);
 }
 
-/* */
+/* 
+2. Parity
+This program uses the modulo operator `%`, which calculates the remainder after division of one number by another. It's often used to assess number parity.
+*/
+
+console.log(10 % 2); // 0 because 10 = 5 * 2 + 0
+console.log(11 % 2); // 1 because 11 = 5 * 2 + 1
+console.log(18 % 3); // 0 because 18 = 3 * 6 + 0
+console.log(19 % 3); // 1 because 19 = 3 * 6 + 1
+console.log(20 % 3); // 2 because 20 = 3 * 6 + 2
+
+/* 
+Improve the program so that it also shows odd numbers. Improve it again to replace the initial number `1` by a number given by the user.
+
+This program must show exactly 10 numbers including the first one, not 11 numbers!
+*/
