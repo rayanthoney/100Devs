a4114034e1fa26f3729f30782a5abd51f2836834
Ray Anthoney <rmaxey911@gmail.com>
2022-03-11 16:02:29 -0700

    100Devs TJSWay chapter 2 completed

--- the-javascript-way/resources/code-skeleton/thejsway-code/chapter02/js/examples.js
+++ the-javascript-way/resources/code-skeleton/thejsway-code/chapter02/js/examples.js
@@ -6,3 +6,37 @@ let b = 0;
 b += 1;
 b++;
 console.log(b);
+
+// new way
+const country = "America";
+console.log(`I live in ${country}`);
+const x = 3;
+const y = 7;
+console.log(`${x} + ${y} = ${x + y}`);
+
+const f = 100;
+console.log("Variable f contains the value " + f);
+
+const g = "five" * 2;
+console.log(g);
+
+const h = "5";
+console.log(h + 1);
+const i = Number("5");
+console.log(i + 1);
+
+// const fullName = prompt("Enter your first name:");
+// alert(`Hello, ${fullName}`);
+
+const temp1 = 36.9;
+const temp2 = 37.6;
+const temp3 = 37.1;
+console.log(temp1, temp2, temp3);
+
+const input = prompt("Enter a number:");
+const nb = Number(input);
+
+const radius = 5.5;
+const pi = 3.14;
+const perimeter = 2 * pi * radius;
+console.log(perimeter);
