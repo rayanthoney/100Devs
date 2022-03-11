c2829c98274d03f42608957a2b03b5ae22876ca5
Ray Anthoney <rmaxey911@gmail.com>
2022-03-10 20:55:51 -0700

    100Devs TJSWay chapter 1 & 2 practice stopped at Variable Scope

--- the-javascript-way/resources/code-skeleton/thejsway-code/chapter01/js/examples.js
+++ the-javascript-way/resources/code-skeleton/thejsway-code/chapter01/js/examples.js
@@ -1,1 +1,31 @@
 // JavaScript code goes here
+
+/*
+console.log("Hello from Javascript");
+
+console.log("Let's do some math");
+
+console.log(4 + 7);
+
+console.log(12 / 0);
+
+console.log("Goodbye!");
+*/
+
+/* 
+let firstName = "Ray Anthoney";
+console.log(firstName);
+
+console.log(54);
+*/
+
+/*
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+*/
+
+console.log(4 + 5); // output is 9
+console.log("4 + 5"); // output is 4 + 5
+console.log("4" + "5"); // output is 45
--- the-javascript-way/resources/code-skeleton/thejsway-code/chapter02/js/examples.js
+++ the-javascript-way/resources/code-skeleton/thejsway-code/chapter02/js/examples.js
@@ -1,1 +1,8 @@
 // JavaScript code goes here
+
+console.log("Let's get into some JavaScript");
+
+let b = 0;
+b += 1;
+b++;
+console.log(b);
