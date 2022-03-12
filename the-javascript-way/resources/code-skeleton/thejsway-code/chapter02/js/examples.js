ef3a09b1d6c27968f56318065c9f28c0f07e7f96
Ray Anthoney <rmaxey911@gmail.com>
2022-03-11 20:47:47 -0700

    100Devs TJSWay completed examples

--- the-javascript-way/resources/code-skeleton/thejsway-code/chapter02/js/examples.js
+++ the-javascript-way/resources/code-skeleton/thejsway-code/chapter02/js/examples.js
@@ -1,45 +1,75 @@
 // JavaScript code goes here
 
-console.log("Let's get into some JavaScript");
+// console.log("Let's get into some JavaScript");
 
-let b = 0;
-b += 1;
-b++;
-console.log(b);
+// let b = 0;
+// b += 1;
+// b++;
+// console.log(b);
 
-const country = "America";
-console.log(`I live in ${country}`);
-const x = 3;
-const y = 7;
-console.log(`${x} + ${y} = ${x + y}`);
+// const country = "America";
+// console.log(`I live in ${country}`);
+// const x = 3;
+// const y = 7;
+// console.log(`${x} + ${y} = ${x + y}`);
 
-const f = 100;
-console.log("Variable f contains the value " + f);
+// const f = 100;
+// console.log("Variable f contains the value " + f);
 
-const g = "five" * 2;
-console.log(g);
+// const g = "five" * 2;
+// console.log(g);
 
-const h = "5";
-console.log(h + 1);
-const i = Number("5");
-console.log(i + 1);
+// const h = "5";
+// console.log(h + 1);
+// const i = Number("5");
+// console.log(i + 1);
 
 // const fullName = prompt("Enter your first name:");
 // alert(`Hello, ${fullName}`);
 
-const temp1 = 36.9;
-const temp2 = 37.6;
-const temp3 = 37.1;
-console.log(temp1, temp2, temp3);
+// const temp1 = 36.9;
+// const temp2 = 37.6;
+// const temp3 = 37.1;
+// console.log(temp1, temp2, temp3);
 
-const input = prompt("Enter a number:");
-const nb = Number(input);
+// const input = prompt("Enter a number:");
+// const nb = Number(input);
 
-const radius = 5.5;
-const pi = 3.14;
-const perimeter = 2 * pi * radius;
-console.log(perimeter);
+// const radius = 5.5;
+// const pi = 3.14;
+// const perimeter = 2 * pi * radius;
+// console.log(perimeter);
 
-const firstName = "Ray";
-const lastName = "Maxey";
-console.log(`Hello!, ${(firstName, lastName)} `);
+// const firstName = "Ray";
+// const lastName = "Maxey";
+
+// console.log("Hello my name is ", firstName, lastName);
+
+// const fullName = prompt("Enter your first and last name:");
+// alert(`Hello, ${fullName}`);
+
+// // Ask the user for the price.
+// let rawPrice = prompt("How much was that?");
+// // Calculate the VAT.
+// let priceVat = (Number(rawPrice) / 100) * 20.6;
+// // Calculate the price + VAT
+// let totalCost = Number(rawPrice) + priceVat;
+// // Let the user know the price + VAT
+// alert(`The total cost will be ${totalCost}`);
+
+// Ask the user for the Celsius teperature
+// let celsius = prompt("Hello what is the temp in Celsius");
+// celsius = Number(celsius);
+// let fahrenheit = (celsius * 9) / 5 + 32;
+// fahrenheit = fahrenheit.toFixed(1);
+// alert(celsius + " degrees celsius is " + fahrenheit + " degrees Fahrenheit.");
+
+let number1 = 5;
+let number2 = 3;
+
+// TODO: type your code here (and nowhere else!)
+number1 = 5 - 2;
+number2 = 3 + 2;
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
