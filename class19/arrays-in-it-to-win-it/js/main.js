b129f79ad3e8b93258b15429401e79884b39b431
Ray Anthoney <rmaxey911@gmail.com>
2022-03-15 20:54:29 -0600

    100Devs added class 19 material JS Array's practice homework

--- class19/arrays-in-it-to-win-it/js/main.js
+++ class19/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,12 @@
 //Create an array of movies with at least three movies.
+let movies = ["Shrek1", "Shrek2", "Shrek3"];
 
 //Using the array from above, store the first movie in a variable
-
+let firstMovie = movies[0];
 //Get the length of the original array and store it in a new variable
-
+let arrLength = movies.length;
+console.log(s.length);
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastElement = movies[2];
+
+let lastElement = movies[movies.length - 1];
