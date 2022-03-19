f457b96d1c937c19e5e4020994b4d4f25ec43a4f
Ray Anthoney <rmaxey911@gmail.com>
2022-03-19 13:14:42 -0600

    100Devs TJSW practice and prep for Anki cards

--- the-javascript-way/resources/code-skeleton/thejsway-code/chapter01/js/examples.js
+++ the-javascript-way/resources/code-skeleton/thejsway-code/chapter01/js/examples.js
@@ -1,30 +1,34 @@
 // JavaScript code goes here
 
-/*
+// Values, Types, Program structure.
+
+// Anki definitions
+//Number
+//Strings
+//Arithmetic
+//Console.log
+//
+
 console.log("Hello from Javascript");
 
-console.log("Let's do some math");
+console.log("Let's do \n some math");
 
 console.log(4 + 7);
+console.log("Hel" + "lo");
 
 console.log(12 / 0);
 
 console.log("Goodbye!");
-*/
 
-/* 
 let firstName = "Ray Anthoney";
 console.log(firstName);
 
 console.log(54);
-*/
 
-/*
 console.log(6 + 3);
 console.log(6 - 3);
 console.log(6 * 3);
 console.log(6 / 3);
-*/
 
 console.log(4 + 5); // output is 9
 console.log("4 + 5"); // output is 4 + 5
