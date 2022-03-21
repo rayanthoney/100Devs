c0b8b2869e60288a8396931e2335ec4266a94c9c
Ray Anthoney <rmaxey911@gmail.com>
2022-03-20 19:40:06 -0600

    100Devs MayanWolfe wordle game build gave on screen keyboard function

--- homework-layouts/Wordle/js/script.js
+++ homework-layouts/Wordle/js/script.js
@@ -8,6 +8,7 @@ let nextLetter = 0; // index of an array starts at 0.
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
 console.log(rightGuessString);
 
+// initBoard function
 function initBoard() {
 	let board = document.getElementById("game-board");
 
@@ -51,6 +52,7 @@ document.addEventListener("keyup", (e) => {
 	}
 });
 
+// insertFunction
 function insertLetter(pressedKey) {
 	if (nextLetter === 5) {
 		return;
@@ -64,3 +66,114 @@ function insertLetter(pressedKey) {
 	currentGuess.push(pressedKey);
 	nextLetter += 1;
 }
+
+// Check deleteLetter function
+function deleteLetter() {
+	let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+	let box = row.children[nextLetter - 1];
+	box.textContent = "";
+	box.classList.remove("filled-box");
+	currentGuess.pop();
+	nextLetter -= 1;
+}
+
+// Check checkGuess function
+function checkGuess() {
+	let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+	let guessString = "";
+	let rightGuess = Array.from(rightGuessString);
+
+	for (const val of currentGuess) {
+		guessString += val;
+	}
+
+	if (guessString.length != 5) {
+		alert("Not enough letters!");
+		return;
+	}
+
+	if (!WORDS.includes(guessString)) {
+		alert("Word not in list!");
+		return;
+	}
+
+	for (let i = 0; i < 5; i++) {
+		let letterColor = "";
+		let box = row.children[i];
+		let letter = currentGuess[i];
+
+		let letterPosition = rightGuess.indexOf(currentGuess[i]);
+		// is letter in the correct guess
+		if (letterPosition === -1) {
+			letterColor = "grey";
+		} else {
+			// now, letter is definitely in word
+			// if letter index and right guess index are the same
+			// letter is in the right position
+			if (currentGuess[i] === rightGuess[i]) {
+				// shade green
+				letterColor = "green";
+			} else {
+				// shade box yellow
+				letterColor = "yellow";
+			}
+
+			rightGuess[letterPosition] = "#";
+		}
+
+		let delay = 250 * i;
+		setTimeout(() => {
+			//shade box
+			box.style.backgroundColor = letterColor;
+			shadeKeyBoard(letter, letterColor);
+		}, delay);
+	}
+
+	if (guessString === rightGuessString) {
+		alert("You guessed right! Game over!");
+		guessesRemaining = 0;
+		return;
+	} else {
+		guessesRemaining -= 1;
+		currentGuess = [];
+		nextLetter = 0;
+
+		if (guessesRemaining === 0) {
+			alert("You've run out of guesses! Game over!");
+			alert(`The right word was: "${rightGuessString}"`);
+		}
+	}
+}
+
+function shadeKeyBoard(letter, color) {
+	for (const elem of document.getElementsByClassName("keyboard-button")) {
+		if (elem.textContent === letter) {
+			let oldColor = elem.style.backgroundColor;
+			if (oldColor === "green") {
+				return;
+			}
+
+			if (oldColor === "yellow" && color !== "green") {
+				return;
+			}
+
+			elem.style.backgroundColor = color;
+			break;
+		}
+	}
+}
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+	const target = e.target;
+
+	if (!target.classList.contains("keyboard-button")) {
+		return;
+	}
+	let key = target.textContent;
+
+	if (key === "Del") {
+		key = "Backspace";
+	}
+
+	document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+});
