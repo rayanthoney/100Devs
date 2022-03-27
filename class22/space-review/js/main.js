586b0f9c65b510318baf70f689871c53885138b1
Ray Anthoney <rmaxey911@gmail.com>
2022-03-27 12:37:41 -0600

    100Devs Office Hours Arrays and Objects discussion

--- class22/space-review/js/main.js
+++ class22/space-review/js/main.js
@@ -1,9 +1,24 @@
 //Arrays
 
+
+
+
+
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+
+// let nums = [42,19,77,33,57]
+
+// let sum = nums.reduce( (acc,c) => acc + c, 0)
+// alert(sum)
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+let nums = [42,19,77,33,57]
+
+let numsSquared = nums.map( n => n * n)
+console.log(numsSquared)
+
+
 
 //Create a function that takes string
 //Print the reverse of that string to the console
