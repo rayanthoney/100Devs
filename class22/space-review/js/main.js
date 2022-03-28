b32570a2bf8b872d768b8fc6ddc2840ec7ab4cdf
Ray Anthoney <rmaxey911@gmail.com>
2022-03-27 20:57:04 -0600

    100Devs Office Hours Array practice

--- class22/space-review/js/main.js
+++ class22/space-review/js/main.js
@@ -1,9 +1,5 @@
 //Arrays
 
-
-
-
-
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
 // let nums = [42,19,77,33,57]
@@ -13,15 +9,27 @@
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-let nums = [42,19,77,33,57]
+let nums = [42, 19, 77, 33, 57];
 
-let numsSquared = nums.map( n => n * n)
-console.log(numsSquared)
-
-
+let numsSquared = nums.map((n) => n * n);
+console.log(numsSquared);
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+// function reverseStr(str) {
+// 	let reverseStr = str.split('').reverse()
+// 	console.log(reverseStr);
+// }
+// reverseStr('car')
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+function isPalindrome(str){
+    let reverseStr = str.split('').reverse().join('')
+    if( str.toLowerCase() === reverseStr.toLowerCase() ){
+        alert('Yes')
+    } else {
+        alert('No')
+    }
+}
