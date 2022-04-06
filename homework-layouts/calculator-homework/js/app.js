179b3e9b1e49a7386fbf70b478d78c82e5e7ca7b
Ray Anthoney <rmaxey911@gmail.com>
2022-04-06 07:36:26 -0600

    100Devs calculator-homework adding to the JS

--- homework-layouts/calculator-homework/js/app.js
+++ homework-layouts/calculator-homework/js/app.js
@@ -1,55 +1,56 @@
 // JavaScrippt goes here
 
-const keys = document.querySelector('.calculator-buttons');
-    keys.addEventListener('click', event => {
-        const {target} = event;
-        const {value} = target;
-        if (!target.matches('button')) {
-            return;
-        } else {
-            calculator.parseInput(value)
-            // console.log(event)
-        }
-    })
+const keys = document.querySelector(".calculator-buttons");
+keys.addEventListener("click", (event) => {
+	const { target } = event;
+	const { value } = target;
+	if (!target.matches("button")) {
+		return;
+	} else {
+		calculator.parseInput(value);
+		// console.log(event)
+	}
+});
 
 const calculator = {
-    displayText: '0',
-    prevTotal: null,
+	displayText: "0",
+	prevTotal: null,
 
-    parseInput(value) {
+	parseInput(value) {
+		// have any of the "special buttons" been clicked?
+		switch (value) {
+			case "=":
+				// calculate the answer
+				break;
+			case "AC":
+				// clear screen and stored values
+				break;
+			case ".":
+				if (this.displayText == 0) {
+					addText(value);
+				} else {
+					// add value to text string
+				}
+				break;
+			default:
+				// add value to text string
+				break;
+		}
+	},
 
-        // have any of the "special buttons" been clicked?
-        switch (value) {
-            case '=' :
-                // calculate the answer
-                break;
-            case 'AC' :
-                // clear screen and stored values
-                break;
-            case '.' :
-                if (this.displayText == 0) {
-                    // pass '0.' into add text method
-                } else {
-                    // add value to text string
-                }
-                break;
-            default:
-                // add value to text string
-                break;
-        }
-    },
-
-    addText(value) {
-        if (this.displayText === '0') {
-            this.displayText = ''
-        } else if (this.prevTotal !== null) {
-            this.displayText = this.prevTotal
-            this.prevTotal = null
-        }
-        if (/* user has entered an invalid sequence don't proceed */) {
-
-        }
-        this.displayText += value
-        // output display text to screen
-    },
-}
+	addText(value) {
+		if (this.displayText === "0") {
+			this.displayText = "";
+		} else if (this.prevTotal !== null) {
+			this.displayText = this.prevTotal;
+			this.prevTotal = null;
+		}
+		if (isNaN(+value) && isNaN(+this.displayText)) {
+			if (isNaN(this.displayText.slice(-1))) {
+				return;
+			}
+		}
+		this.displayText += value;
+		// output display text to screen
+	},
+};
