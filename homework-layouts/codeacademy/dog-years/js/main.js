7173099429fd6d0f28527de57c5b72cefae10b1d
Ray Anthoney <rmaxey911@gmail.com>
2022-04-09 15:25:07 -0600

    100Devs CodeAcademy javascript Dog Years completed

--- homework-layouts/codeacademy/dog-years/js/main.js
+++ homework-layouts/codeacademy/dog-years/js/main.js
@@ -1,15 +1,21 @@
 console.log("Hello 100Devs, Lets do some JavaScript");
 
-// //
-// // create a variable with my current age.
-// const myAge = 54;
-// // create a variable with early Years.
-// let earlyYears = 2;
-// //
-// earlyYears *= 10.5
-// //
-// let earlyYears = myAge - 2;
-// laterYears *= 4;
-// console.log(earlyYears);
-// console.log(laterYears);
+//
+// create a variable with my current age.
+const myAge = 54;
+// create a variable with early Years.
+let earlyYears = 2;
+//
+earlyYears *= 10.5
+//
+let laterYears = myAge - 2;
+laterYears *= 4;
 
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'RayAnthoney'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in Dog Years`);
