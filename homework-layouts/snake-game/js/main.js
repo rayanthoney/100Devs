cad41beef22a965312cc275202408ec563f74ffe
Ray Anthoney <rmaxey911@gmail.com>
2022-04-25 17:47:22 -0600

    100Devs  completed html css pages

--- homework-layouts/snake-game/js/main.js
+++ homework-layouts/snake-game/js/main.js
@@ -1,1 +1,30 @@
-console.log("Hello 100Devs, Lets do some JavaScript");
+// DECLARE GLOBAL VARIABLES TO TRACK GAME BOARD SIZE
+const LINE_PIXEL_COUNT = 40
+const TOTAL_PIXEL_COUNT = LINE_PIXEL_COUNT**2
+
+// TRACK SCORES TO DISPLAY TO USER
+let totalFoodEaten = 0
+let totalDistanceTraveled = 0
+
+// SHORTEN REFERENCE TO GAME BOARD
+const gameContainer = document.getElementById('game-container')
+
+// GENERATE THE GAME BOARD
+const createGameBoardPixels = () => {
+    for (let i = 1; i<=TOTAL_PIXEL_COUNT; i++) {
+        gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="game-board__pixel" id = "pixel${i}"></div>`
+    }
+}
+
+// SHORTEN OUR REFERENCE TO GAME PIXELS
+const gameBoardPixels = document.getElementsByClassName('game-board__pixel')
+
+let currentFoodPosition = 0
+
+//create the randomly generated food items in the game board
+const createFood = () => {
+    gameBoardPixels[currentFoodPosition].classList.remove("food")
+currentFoodPosition = Math.floor(Math.random()*TOTAL_PIXEL_COUNT)
+gameBoardPixels[currentFoodPosition].classList.add('food')
+}
+
