e3a1c4e2908c46e3d67b906fe2e1b495e9ad8696
Ray Anthoney <rmaxey911@gmail.com>
2022-04-30 19:56:46 -0600

    100Devs mosh js-oop tutorial 25min

---
+++ homework-layouts/JS-OOP/index.js
@@ -0,0 +1,46 @@
+// JAVASCRIPT GOES HERE
+console.log("Hello World!");
+
+// OBJECTS
+// CIRCLE IS AN OBJECT
+// const circle = {
+//     // RADIUS, LOCATION, X, Y ARE ALL PROPERTIES (HOLDING VALUES)
+//     // THESE ARE NOT METHODS WHICH WOULD (DEFINE LOGIC)
+// 	radius: 1,
+// 	location: {
+// 		x: 1,
+// 		y: 1,
+// 	},
+//     // DRAW IS A METHOD
+// 	draw: function () {
+// 		console.log("draw");
+// 	},
+// };
+// // DOT NOTATION
+// circle.draw();
+
+// FACTORY FUNCTION
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw: function() {
+//             console.log('draw');
+//         }
+//     };
+// }
+// const circle = createCircle(1);
+// // circle.draw();
+
+// // CONSTRUCTOR FUNCTION
+// // this. and new REFER TO A CONSTRUCTOR FUNCTION
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+// const another = new Circle(1);
+
+// CONSTRUCTOR PROPERTY
+let x = {};
+
