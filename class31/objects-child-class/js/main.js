2177debae71c1cacd9a62f1330d78c15a6f22805
Ray Anthoney <rmaxey911@gmail.com>
2022-05-07 16:02:55 -0600

    100Devs class32 material

--- class31/objects-child-class/js/main.js
+++ class31/objects-child-class/js/main.js
@@ -1,2 +1,13 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class Animal {
+	constructor(name) {
+		this._name = name;
+	}
+	get name() {
+		return this._name;
+	}
+	speak() {
+		console.log(`${this._name} makes a sound`);
+	}
+}
