7f4e2626ae8ae085bfa8c605180d185e3f0581c1
Ray Anthoney <rmaxey911@gmail.com>
2022-05-14 13:15:40 -0600

    100Devs node crash course url usage

---
+++ homework-layouts/node-crash-course/reference/url_demo.js
@@ -0,0 +1,31 @@
+const url = require("url");
+
+const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
+
+// SERIALIZED URL
+console.log(myUrl.href);
+
+// TO STRING METHOD SERIALIZED URL
+console.log(myUrl.toString());
+
+// HOST (ROOT DOMAIN)
+console.log(myUrl.host);
+
+// HOSTNAME
+console.log(myUrl.hostname);
+
+// PATHNAME
+console.log(myUrl.pathname);
+
+// SERIALIZED QUERY
+console.log(myUrl.search);
+
+// PARAMETERS OBJECT
+console.log(myUrl.searchParams)
+
+// ADD PARAMETERS
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams)
+
+// LOOP THROUGH PARAMETERS
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`))
