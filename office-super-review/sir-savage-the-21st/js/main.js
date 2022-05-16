308ea5cd6b0da43196ebd397e668902d43fdaaeb
Ray Anthoney <rmaxey911@gmail.com>
2022-05-16 06:27:14 -0600

    100Devs clean up from superreview and 100Hours

--- office-super-review/sir-savage-the-21st/js/main.js
+++ office-super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,8 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+function sirSavageThe21st() {
+    for (let i = 1; i <= 21; i++) {
+        // console.log('21')
+        document,quer
+    }
+}
