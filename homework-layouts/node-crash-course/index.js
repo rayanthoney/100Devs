3da19177f5495bce4d495ef6adeeba8cb7228a2d
Ray Anthoney <rmaxey911@gmail.com>
2022-05-16 13:41:39 -0600

    node-crash-course rewrite

--- homework-layouts/node-crash-course/index.js
+++ homework-layouts/node-crash-course/index.js
@@ -22,20 +22,77 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	if(req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-        })
-    }
-	if(req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-        })
-    }
+	// if (req.url === "/") {
+	// 	fs.readFile(
+	// 		path.join(__dirname, "public", "index.html"),
+	// 		(err, content) => {
+	// 			if (err) throw err;
+	// 			res.writeHead(200, { "Content-Type": "text/html" });
+	// 			res.end(content);
+	// 		}
+	// 	);
+	// }
+	// if (req.url === "/api/users") {
+	// 	const users = [
+	// 		{ name: "Don Bobolina", age: 40 },
+	// 		{ name: "Stan Smith", age: 30 },
+	// 	];
+	//     res.writeHead(200, { "Content-Type": "application/json" });
+	//     res.end(JSON.stringify(users));
+	// }
+
+	// BUILD FILE PATH
+	let filePath = path.join(
+		__dirname,
+		"public",
+		req.url === "/" ? "index.html" : req.url
+	);
+
+	// GET THE EXTENSION OPF A FILE
+	let extname = path.extname(filePath);
+
+	// SET THE INITIAL CONTENT TYPE
+	let contentType = "text/html";
+
+	// CHECK EXTENSION AND SET CONTENT TYPE
+	switch (extname) {
+		case ".js":
+			contentType = "text/javascript";
+			break;
+		case ".css":
+			contentType = "text/css";
+			break;
+		case ".json":
+			contentType = "application/json";
+			break;
+		case ".png":
+			contentType = "image/png";
+			break;
+		case ".jpg":
+			contentType = "image/jpg";
+			break;
+	}
+
+	// READ FILE
+	fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code == 'ENOENT') {
+                // PAGE NOT FOUND
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { "Content-Type": "text/html" });
+                    res.end(content, 'utf8');
+                })
+            } else {
+                // SOME SERVER ERROR
+                res.writeHead(500);
+                res.end(`Server Error: ${er.code}`);
+            }
+        } else {
+            // SUCCESS
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content, 'utf8');
+        }
+    });
 });
 
 const PORT = process.env.PORT || 5000;
