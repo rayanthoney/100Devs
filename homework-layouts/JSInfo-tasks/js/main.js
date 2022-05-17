11702ca02824dca35da571fd6b9f162d1911cba1
Ray Anthoney <rmaxey911@gmail.com>
2022-05-17 14:43:53 -0600

    100Devs refactored

--- homework-layouts/JSInfo-tasks/js/main.js
+++ homework-layouts/JSInfo-tasks/js/main.js
@@ -9,7 +9,6 @@ console.log("Hello 100Devs, Lets do some JavaScript");
 // SHOW AN ALERT WITH AN EXTERNAL SCRIPT:
 // alert("I'm JavaScript!");
 
-
 // alert('Hello'); alert('World');
 
 // alert('Hello')
@@ -28,7 +27,6 @@ console.log("Hello 100Devs, Lets do some JavaScript");
 
 // alert( admin ); // "John"
 
-
 // Create a variable with the name of our planet. How would you name such a variable?
 
 // let ourPlanetName = "Earth";
@@ -36,3 +34,27 @@ console.log("Hello 100Devs, Lets do some JavaScript");
 // Create a variable to store the name of a current visitor to a website. How would you name that variable?
 
 // let currentUserName = "John";
+
+// alert(undefined === 0);
+
+// Tasks:
+// Comparisons:
+// alert(5 > 4) // true
+// alert("🍎" > "🍍") // true
+// alert("apple" > "pineapple") // false - Dictionary comparison, hence false. "a" is smaller than "p".
+// alert("2" > "12") // true - dictionary comparison, first char "2" is greater than the first char "1".
+// alert(undefined == null) // true - Values null and undefined equal each other only.
+// alert(undefined === null) // false
+// alert(null == "\n0\n") // false
+// alert(null === + "\n0\n") // false
+
+
+let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year < 2015) {
+  alert( 'Too early...' );
+} else if (year > 2015) {
+  alert( 'Too late' );
+} else {
+  alert( 'Exactly!' );
+}
