b6e6756a83fa9060b3bdbcf631535f59854bd81f
Ray Anthoney <rmaxey911@gmail.com>
2022-05-17 14:45:34 -0600

    100Devs refactored

---
+++ homework-layouts/codeacademy/If Statements/js/main.js
@@ -0,0 +1,15 @@
+// Using the let keyword, declare a variable named sale. Assign the value true to it.
+let sale = true;
+// Below the sale variable declaration, but before the if statement, reassign sale to false
+  sale = false;
+// Now create an if statement. Provide the if statement a condition of sale
+if (sale) {
+  console.log('Time to buy!')
+}
+
+// Less than: <
+// Greater than: >
+// Less than or equal to: <=
+// Greater than or equal to: >=
+// Is equal to: ===
+// Is not equal to: !==
