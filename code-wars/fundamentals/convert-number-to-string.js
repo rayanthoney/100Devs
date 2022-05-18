abd924e3e22b7ac6a05b4525d52d1616334ef2fe
Ray Anthoney <rmaxey911@gmail.com>
2022-05-18 15:30:22 -0600

    100Devs code-wars convert number to a string

---
+++ code-wars/fundamentals/convert-number-to-string.js
@@ -0,0 +1,26 @@
+// Convert a Number to a String
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// METHOD toString();
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+  }
+
+// // METHOD TERNARY
+//   function numberToString(num) {
+//     // Return a string of the number here!
+//     return `${num}`;
+//   }
+
+//   // METHOD CONCATENATION
+//   function numberToString(num) {
+//     // Return a string of the number here!
+//     let str = '';
+//     dtr = str + num;
+//     return str;
+//   }
