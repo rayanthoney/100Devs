77326686b395ab3034b0e54190081db1c605cb09
Ray Anthoney <rmaxey911@gmail.com>
2022-05-19 16:16:22 -0600

    100Devs added class35 material

---
+++ homework-layouts/api-nasa/js/rocket.js
@@ -0,0 +1,55 @@
+$(function () {
+    var $ship = $('#ship');
+    var SHIP = {
+        vx: 0,
+        vy: 0,
+        pos: {
+            x: 0,
+            y: 0
+        },
+        firing: false,
+        moving: false,
+        rotation: 0
+    }
+
+    var fire = null;
+    $('body').on('click', function () {
+        clearTimeout(fire);
+        $ship.addClass('fire');
+        fire = setTimeout(function () {
+            $ship.removeClass('fire');
+        }, 800);
+    });
+
+    $('#scene').on('click', function (e) {
+        var old_pos = SHIP.pos;
+        console.log(old_pos)
+        $ship.css({
+            top: e.clientY,
+            left: e.clientX
+        });
+
+        SHIP.pos = {
+            x: e.clientX,
+            y: e.clientY
+        }
+
+        // left or right
+        if (old_pos.x > SHIP.pos.x) {
+
+        } else {
+
+        }
+
+        // up  or down
+        if (old_pos.y > SHIP.pos.y) {
+
+        } else {
+
+        }
+
+
+
+
+    });
+});
