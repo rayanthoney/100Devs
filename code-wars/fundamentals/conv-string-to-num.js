93616faaca4677c6f482324b2076ba832cbf2e07
Ray Anthoney <rmaxey911@gmail.com>
2022-05-21 19:51:20 -0600

    100Devs Code war stringToNumber

---
+++ code-wars/fundamentals/conv-string-to-num.js
@@ -0,0 +1,20 @@
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+
+const stringToNumber = function(str){
+    // put your code here
+    return Number(str);
+  }
+
+
+//   const stringToNumber = str => +str;
+
+
+//   var stringToNumber = function(str){
+//     return parseInt(str);
+//   }
