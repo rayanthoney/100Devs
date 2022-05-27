99e420fe404c6d6839e1bb09ce5574b6b7eff48b
Ray Anthoney <rmaxey911@gmail.com>
2022-05-27 15:38:21 -0600

    100Devs added class37 material

--- class37/node-backend-simple-json/server.js
+++ class37/node-backend-simple-json/server.js
@@ -1,13 +1,16 @@
+// MODULES | HEAVY LIFTERS | Requires
 const http = require('http');
 const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
+// CREATES AN HTTP SERVER | HANDELERS
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
+
   if (page == '/') {
     fs.readFile('index.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
