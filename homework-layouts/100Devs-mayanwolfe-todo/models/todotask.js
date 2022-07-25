92526173dea65c1e219304c8e39a4c0dfdc587c2
Ray Anthoney <rmaxey911@gmail.com>
2022-07-24 19:37:50 -0600

    Created index.js

---
+++ homework-layouts/100Devs-mayanwolfe-todo/models/todotask.js
@@ -0,0 +1,17 @@
+// SCHEMA/BLUEPRINT
+const mongoose = require('mongoose')
+const todoTaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required:true
+    },
+    content: {        
+            type: String,
+            required:true    
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model('TodoTask', todoTaskSchema, 'tasks')
--- homework-layouts/100Devs-mayanwolfe-todo/server.js
+++ homework-layouts/100Devs-mayanwolfe-todo/server.js
@@ -6,21 +6,37 @@
 
 
 // declare variables
-const express = require('express')
-
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = 8500;
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+
 // ADD MODEL VARIABLE
+const TodoTask = require('./models/todotask')
+
+require('dotenv').config();
+
+
 
 // MIDDLEWARE (TELLS SERVER WHERE TO LOOK FOR STUFF)
-
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, () => {console.log('We are connected to the db!')})
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true }, () => {console.log('We are connected to the db!');})
 
 
+// GET METHOD
+app.get('/', async (req, res) => {
+    try {
+        TodoTask.find({}, (err, tasks) => {
+            res.render('index.ejs', {todoTasks: tasks})
+        } )
+    } catch (err) {
+        if (err) return res.status(500).send(err)
+    }
+})
+
+
+// START SERVER
 app.listen(PORT, () => console.log(`Server is running on port! ${PORT} You Betta go and catch it!`))
