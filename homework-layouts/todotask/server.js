de4f1d7655904b96a40bfbe8d91079726a9723fc
Ray Anthoney <rmaxey911@gmail.com>
2022-07-26 20:07:52 -0600

    Created new folder

--- homework-layouts/todotask/server.js
+++ homework-layouts/todotask/server.js
@@ -1,20 +1,36 @@
 // DECLARE VARIABLES
-const express = require('express')
-const app = express()
-const PORT = 8500
-const mongoose = require('mongoose')
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const PORT = 8500;
+const mongoose = require("mongoose");
+const todotask = require("./models/todotask");
+require("dotenv").config();
 
 // ADD MODEL VARIABLE
-// const TodoTask = require('./models/todoask')
+// const TodoTask = require('./models/todotask')
 
 // ADD MIDDLEWARE (TELLS SERVER WHERE TO LOOK FOR STUFF!)
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+// SET THE VIEW ENGINE TO EJS
+app.set(`view engine', 'ejs`);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, () => {console.log('We are connected to the db!')} )
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("We are connected to the db!");
+});
 
+// GET METHOD
+app.get("/", async (req, res) => {
+  try {
+    TodoTask.find({}, (err, tasks) => {
+      res.render("index.ejs", { todoTask: tasks });
+    });
+  } catch (err) {
+    if (err)return res.status(500).send(err)
+  }
+});
 
 // START SERVER
-app.listen(PORT, () => console.log(`Server is running on port! ${PORT} You Betta go and catch it!`))
+app.listen(PORT, () =>
+  console.log(`Server is running on port! ${PORT} You Betta go and catch it!`)
+);
