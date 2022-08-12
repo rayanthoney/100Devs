386353ded9b0d5f267aa5924404e53b10b198554
Ray Anthoney <rmaxey911@gmail.com>
2022-08-12 05:18:56 -0600

    100Devs storybooks project app js handlebars

--- homework-layouts/tm-storybooks-project/app.js
+++ homework-layouts/tm-storybooks-project/app.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
+
+
 // LOAD CONFIG 
-dotenv.config(  {   path: "./config/config.env"  })
+dotenv.config({ path: "./config/config.env" })
 
 
 // CONNECTING TO DATABASE
-// connectDB()
+connectDB()
 
 // INITIALIZE EXPRESS APP
 const app = express()
 
-const PORT = process.env.PORT || 8500
+// LOGGING
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// HANDLEBARS
+app.engine('.hbs', exphbs.engine(
+    {defaultLayout: 'main', extname: '.hbs'}
+    ))
+app.set('view engine', '.hbs')
+// app.set('views', './views');
+
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}! Betta go catch it!`))
 
