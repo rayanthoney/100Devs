65dbec8919bf188f5f0e5ddf457a37a3ae7a516a
Ray Anthoney <rmaxey911@gmail.com>
2022-09-10 07:13:19 -0600

    100Devs menu tracking completed in login controller

--- homework-layouts/100Devs-mw-menu-tracking/controllers/login.js
+++ homework-layouts/100Devs-mw-menu-tracking/controllers/login.js
@@ -39,5 +39,35 @@ exports.postLogin = async (req, res) => {
             });
           }
         });
+      } if (loginSuccess === true) {
+        res.redirect('/')
+      } else {
+        res.render('login', {title: 'login', loggedIn: false, error: 'Invalid Login!'});
+      }
+      exports.postSignup= async (req, res) => {
+        let email = rq.body.emailInput
+        let pass = req.body.pwdInput
+
+        if (email !== '' && pass !== '') {
+          let users= schemas.users
+          let qry = {email:email}
+
+          let userSearch = await users.findOne(qry)
+          .then(async(data) => {
+            if (!data ) {
+              let saltRounds = 10
+              let passSalt = await bcrypt.genSalt(saltRounds, async(err, salt) => {
+                let passHash = await bcrypt.hash(pass, salt, async(err, hash) => {
+                  let acct = {email:email, pwd:hash, level: 'admin'}
+                  let newUser = new schemas.users(acct)
+                  let saveUser = await newUser.save()
+                })
+              })
+            }
+          })
+          res.render('login', {title:'login', loggedIn:false, error:'Please login with your new account'})
+        } else {
+          res.render('new-acct', {title:'New Account', loggedIn:false, error:'All fields are required. Please check and try again'})
+        }
       }
     }
