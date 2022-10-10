c2c9e3cf7eafb6243ee5cc8617bd0ef3cf90598c
Ray Anthoney <rmaxey911@gmail.com>
2022-10-10 06:30:35 -0600

    100Devs react task tracker App.js

--- homework-layouts/tm-react-task-tracker/src/App.js
+++ homework-layouts/tm-react-task-tracker/src/App.js
@@ -29,15 +29,19 @@ const App = () => {
 
 // Delete Task
 const deleteTask = (id) => {
-  console.log('delete', id)
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
 }
 
-  return (
-    <div className="container">
-      <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
-    </div>
-  );
+return (
+  <div className="container">
+    <Header />
+    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No tasks to display"}
+  </div>
+);
 }
 
 // This is how to create a class in older versions of React..
