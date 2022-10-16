2f686220ba28907e7ae31abd6514649db68f8718
Ray Anthoney <rmaxey911@gmail.com>
2022-10-15 21:01:30 -0600

    Anki added a coderunner js

--- Anki/coderunner.js
+++ Anki/coderunner.js
@@ -1,8 +1,9 @@
-let text1 = "Sample Text"
-console.log(1, text1.charAt(0))
-console.log(2, text1.charAt(text1.length-1))
-console.log(3, text1.charAt("0"))
-console.log(4, text1.charAt("test"))
-let test1 = ''
-console.log(5, test1.charAt(""))
+function reverseString(text){
+    let result = "";
 
+    for(let char of text){
+        result = char + result
+    }
+
+    return result;
+}
