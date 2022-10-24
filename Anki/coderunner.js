78157774be18a3ffbb112af58f44ee8e5e0dfdc3
Ray Anthoney <rmaxey911@gmail.com>
2022-10-23 19:20:22 -0600

    100Devs anki cards practice w/coderunner

--- Anki/coderunner.js
+++ Anki/coderunner.js
@@ -4,3 +4,15 @@ const result = words.filter(word => word.length > 6);
 
 console.log(result);
 // expected output: Array ["exuberant", "destruction", "present"]
+
+
+
+const str = 'Hello world! My name is also world. How funny.';
+const stringPattern = 'world';
+const regexPattern = /world/gi;
+const replacement = 'gitconnected';
+
+console.log(str.replace(stringPattern, replacement));
+// returned output: "Hello gitconnected! My name is also world. How funny."
+console.log(str.replace(regexPattern, replacement));
+// returned output:  "Hello gitconnected! My name is also gitconnected. How funny."
