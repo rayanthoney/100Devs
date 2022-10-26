6e66ce6844343edb2f973dfba554dbcc14225eb1
Ray Anthoney <rmaxey911@gmail.com>
2022-10-26 06:23:44 -0600

    100Devs Anki arrays method practice

--- Anki/coderunner.js
+++ Anki/coderunner.js
@@ -1,18 +1,6 @@
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const array1 = [5, 12, 8, 130, 44];
 
-const result = words.filter(word => word.length > 6);
+const found = array1.find(element => element > 10);
 
-console.log(result);
-// expected output: Array ["exuberant", "destruction", "present"]
-
-
-
-const str = 'Hello world! My name is also world. How funny.';
-const stringPattern = 'world';
-const regexPattern = /world/gi;
-const replacement = 'gitconnected';
-
-console.log(str.replace(stringPattern, replacement));
-// returned output: "Hello gitconnected! My name is also world. How funny."
-console.log(str.replace(regexPattern, replacement));
-// returned output:  "Hello gitconnected! My name is also gitconnected. How funny."
+console.log(found);
+// expected output: 12
