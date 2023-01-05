34cf3f4be70766120295b4483de1043301cc7868
Ray Anthoney <rmaxey911@gmail.com>
2023-01-04 22:44:55 -0700

    added standup folder to follow along

---
+++ code-wars/standup/4loop-recursion.js
@@ -0,0 +1,52 @@
+// Stepping Through Recursive Fibonacci Function (#kyu)
+
+// Instructions 
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+
+```md
+P R E P Method   =>
+
+|     |     |                  |
+| --- | --- | ---------------- |
+|     | P   | Parameters       |
+|     | R   | Results          |
+|     | E   | Examples         |
+|     | P   | Psuedocode       |
+```
+## Parameters: 
+
+## Result: 
+
+## Examples: 
+// https://www.youtube.com/watch?v=zg-ddPbzcKM
+// https://youtu.be/vYquumk4nWw
+// https://www.youtube.com/watch?v=hISXxjX6Ino
+```js
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+    Given n, calculate F(n).
+
+    Example 1:
+
+    Input: n = 2
+    Output: 1
+    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+    Example 2:
+
+    Input: n = 3
+    Output: 2
+    Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+    Example 3:
+
+    Input: n = 4
+    Output: 3
+    Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+```
+## Psuedocode: 
+
+
+---
+
+
+## CodeWar
