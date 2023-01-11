3d4a79b957dc33fb20b254464976a0b3d9ddfce4
Ray Anthoney <rmaxey911@gmail.com>
2023-01-10 21:04:01 -0700

    added middleware to the server.js

--- homework-layouts/100Devs_2023MVC_CRUD_template/server.js
+++ homework-layouts/100Devs_2023MVC_CRUD_template/server.js
@@ -1,2 +1,28 @@
 // todo - declare Variables::
-const express = require('express')
+const express = require('express');
+var cors = require('cors')
+const app = express();
+const PORT = 8000
+const mongoose =require('mongoose');
+
+    // *Import function/routes
+
+// todo - Connect to Database
+
+// todo - Set Middleware
+
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+// todo - Set Routes
+
+// todo - Start Server
+app.listen(PORT, () => console.log(`Server is running on ${PORT}, you better go catch it!`))
+
+
+
+
+
+
