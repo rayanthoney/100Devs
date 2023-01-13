3bcbdb6284958f8095f45102de87abf36c4f9134
Ray Anthoney <rmaxey911@gmail.com>
2023-01-12 21:55:58 -0700

    created a controller

--- homework-layouts/100Devs_2023MVC_CRUD_template/controllers/home.js
+++ homework-layouts/100Devs_2023MVC_CRUD_template/controllers/home.js
@@ -3,11 +3,24 @@ const ItemList = require("../models/itemlist");
 module.exports = {
   getIndex: async (req, res) => {
     try {
-        const items = await ItemList.find()
-        res.render('index.ejs', {itemList: items})
+      const items = await ItemList.find();
+      res.render("index.ejs", { itemList: items });
     } catch (error) {
-        if (error) return res.status(500).send(error)
+      if (error) return res.status(500).send(error);
     }
   },
-  createItem: async (req, res) => {},
+  createItem: async (req, res) => {
+    const newItem = new ItemList({
+      textInput: req.body.textInput,
+      numInput: req.body.numInput,
+    });
+    try {
+      await newItem.save()
+      console.log(newItem)
+      res.redirect('/')
+    } catch (error) {
+      if (error) return res.status(500).send(error);
+      res.redirect('/')
+    }
+  },
 };
