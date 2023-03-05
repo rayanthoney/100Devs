f613e6494875ef7455436be3934b9b98fb1b37f5
Ray Anthoney <rmaxey911@gmail.com>
2023-03-05 15:27:27 -0700

    add item logic

--- homework-layouts/100Devs-mw-typescript-vite/client/src/main.ts
+++ homework-layouts/100Devs-mw-typescript-vite/client/src/main.ts
@@ -1,18 +1,49 @@
 import { v4 as uuidV4 } from "uuid";
 
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
 const list = document.querySelector<HTMLUListElement>("#list");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 const form = document.getElementById("new-task-form") as HTMLFormElement;
+const tasks: Task[] = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input?.value == "" || input?.value == null) return;
 
-  const newTask = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
   };
+  tasks.push(newTask);
+  saveTask();
+
   addListItem(newTask);
+  input.value = "";
 });
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox";
+  checkbox.checked = checkbox.checked;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  
+}
