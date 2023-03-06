b139856d99351ccf3358fa219006460945129774
Ray Anthoney <rmaxey911@gmail.com>
2023-03-06 05:30:48 -0700

    add item logic

--- homework-layouts/100Devs-mw-typescript-vite/client/src/main.ts
+++ homework-layouts/100Devs-mw-typescript-vite/client/src/main.ts
@@ -45,5 +45,12 @@ function addListItem(task: Task) {
 }
 
 function saveTasks() {
-  
+  localStorage.setItem('TASKS', JSON.stringify(tasks))
 }
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS')
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
+
