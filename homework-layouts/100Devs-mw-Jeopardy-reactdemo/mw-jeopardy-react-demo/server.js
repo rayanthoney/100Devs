6ab576f78228f239c06959d5943f3bbf3d25fb92
Ray Anthoney <rmaxey911@gmail.com>
2023-08-17 21:11:56 -0600

    created server

--- homework-layouts/100Devs-mw-Jeopardy-reactdemo/mw-jeopardy-react-demo/server.js
+++ homework-layouts/100Devs-mw-Jeopardy-reactdemo/mw-jeopardy-react-demo/server.js
@@ -1,29 +1,31 @@
-
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 
-app.get('/test', (req, res) => {
-  res.send('CORS-enabled server is running!');
+app.get("/test", (req, res) => {
+  res.send("CORS-enabled server is running!");
 });
 
 // New route to handle requests to the jservice API
-app.use('/api', async (req, res) => {
-  console.log(`http://jservice.io/api${req.url}`)
+app.use("/api", async (req, res) => {
+  //jservice.io/api/category?id=150
+  https: console.log(`http://jservice.io/api${req.url}`);
   try {
     // Forward the request to the jservice API
     const apiResponse = await fetch(`http://jservice.io/api${req.url}`);
     const data = await apiResponse.json();
     res.send(data);
   } catch (error) {
-    res.status(500).send('Error fetching data from the jservice API.');
+    res.status(500).send("Error fetching data from the jservice API.");
   }
 });
 
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(
+    `Server is running on http://localhost:${PORT}, better go catch it!`
+  );
 });
