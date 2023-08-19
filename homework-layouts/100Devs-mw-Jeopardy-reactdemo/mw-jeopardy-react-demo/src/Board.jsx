59484f68b1b7ce9369719fb9e97bf0a3d23b5497
Ray Anthoney <rmaxey911@gmail.com>
2023-08-18 21:20:36 -0600

    added HTML for the Board.jsx

--- homework-layouts/100Devs-mw-Jeopardy-reactdemo/mw-jeopardy-react-demo/src/Board.jsx
+++ homework-layouts/100Devs-mw-Jeopardy-reactdemo/mw-jeopardy-react-demo/src/Board.jsx
@@ -1,48 +1,49 @@
-import React, {useState, useEffect} from "react";
-import Category from "./Category"
+import React, { useEffect, useState } from "react";
+import Category from "./Category";
+import "./board.css";
 
 function Board() {
-const [categories, setCategories] = useState([])
-    
-// This useEffect hook will run once after the component is rendered.
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            let offset = Math.floor(Math.random() * 28175) = 1
+  // This useEffect hook will run once after the component is first rendered.
+  useEffect(() => {
+    async function fetchData() {
+      let offset = Math.floor(Math.random() * 28175) + 1;
 
-            // Fetch the catogories from the API
+      // Fetch the categories from the API.
+      const categoryUrl = `http://localhost:5000/api/categories?count=6&offset=${offset}`;
 
-            const categoryURL = `https://localhost:6000/api/categories?count=6&offset=${offset}`
-            
-                const categoryResponse = await fetch(categoryURL)
-                const categoryData = await categoryResponse.json()
-            
-            // Fetch the clues for each category
-            const categoriesWithClues = await Promise.all(categoryData.map(async(category) => {
-                const clueURL = `https://localhost:6000/api/clues?category=${category.id}`
-            
-                const clueResponse = await fetch(clueURL)
-                const clueData = await clueResponse.json()
-            
-            // Attach the clues to the category
-            return {
-                ...category,
-                clues: clueData,
-            }
-            }))
-            // Put categories and clues inside of the state variable.
-            setCategories(categoriesWithClues)
+      const categoryResponse = await fetch(categoryUrl);
+      const categoryData = await categoryResponse.json();
 
-        }
+      // Fetch the clues for each category.
+      const categoriesWithClues = await Promise.all(
+        categoryData.map(async (category) => {
+          const clueUrl = `http://localhost:5000/api/clues?category=${category.id}`;
+          const clueResponse = await fetch(clueUrl);
+          const clueData = await clueResponse.json();
 
-        fetchData()
-    },[])
+          // Attach the clues to the category.
+          return {
+            ...category,
+            clues: clueData,
+          };
+        })
+      );
 
-    return (
-        <div className="jeapordy-board">
-            {categories.map((category) => {
-                <Category key={category.id} category={category} />
-            })}
-        </div>
-    )
+      setCategories(categoriesWithClues);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="jeopardy-board">
+      {categories.map((category) => (
+        <Category key={category.id} category={category} />
+      ))}
+    </div>
+  );
 }
+
+export default Board;
